d72a19f79e3892ce1f9be9c83b397c18de8e0234
Abel Binoop <programmabel@outlook.com>
2021-02-03 14:33:01 +0000

    add 69

---
+++ _69.py
@@ -0,0 +1,5 @@
+countries = ("America", "UK", "India", "Australia", "Denmark")
+print(countries)
+
+country = input("Enter the name of one of the countries above: ")
+print(f"That country has an index of {countries.index(country)}")
